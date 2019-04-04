ef30007d4e43a853ff037e9340d4e67967407d9f
Vladimir <hotchenko@gmail.com>
2019-04-04 18:13:41 +0300

    add new samples

---
+++ Asynchronous Export/FormMain.cs
@@ -0,0 +1,70 @@
+﻿using Stimulsoft.Report;
+using System;
+using System.Windows.Forms;
+
+namespace Asynchronous_Export
+{
+    public partial class FormMain : Form
+    {
+        public StiReport Report { get; set; }
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            // How to Activate
+            //Stimulsoft.Base.StiLicense.Key = "6vJhGtLLLz2GNviWmUTrhSqnO...";
+            //Stimulsoft.Base.StiLicense.LoadFromFile("license.key");
+            //Stimulsoft.Base.StiLicense.LoadFromStream(stream);
+
+            Report = GetTemplate();
+        }
+
+        private StiReport GetTemplate()
+        {
+            var report = new StiReport();
+            report.Load("Dashboards\\Christmas.mrt");
+
+            return report;
+        }
+
+        private async void buttonPdf_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = Report.ReportName + ".pdf";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelStatus.Text = "Exporting...";
+
+                await Report.ExportDocumentAsync(StiExportFormat.Pdf, saveFileDialog.FileName);
+
+                labelStatus.Text = "";
+            }
+        }
+
+        private async void buttonExcel_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = Report.ReportName + ".xlsx";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelStatus.Text = "Exporting...";
+
+                await Report.ExportDocumentAsync(StiExportFormat.Excel2007, saveFileDialog.FileName);
+
+                labelStatus.Text = "";
+            }
+        }
+
+        private async void buttonImage_Click(object sender, EventArgs e)
+        {
+            saveFileDialog.FileName = Report.ReportName + ".png";
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                labelStatus.Text = "Exporting...";
+
+                await Report.ExportDocumentAsync(StiExportFormat.ImagePng, saveFileDialog.FileName);
+
+                labelStatus.Text = "";
+            }
+        }
+    }
+}
